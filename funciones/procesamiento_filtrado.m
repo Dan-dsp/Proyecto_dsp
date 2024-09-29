9c473a30f9e77549178a00644970d541f72b5ce0
Dan-dsp <daniel.pelaez@correounivalle.edu.co>
2024-09-29 16:18:43 -0500

    Se añadió el archivo que contendrá el preprocesamiento y se eliminó un archivo que no tenía nada.

--- funciones/procesamiento_filtrado.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = procesamiento_filtrado(inputArg1,inputArg2)
-%PROCESAMIENTO_FILTRADO Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
