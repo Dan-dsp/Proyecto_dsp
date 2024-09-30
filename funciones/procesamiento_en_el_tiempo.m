7101796b95cec89a71cf2e6f858f1975d1a6599e
Dan-dsp <daniel.pelaez@correounivalle.edu.co>
2024-09-29 21:57:26 -0500

    Creación del archivo "procesamiento en el tiempo"

---
+++ funciones/procesamiento_en_el_tiempo.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = procesamiento_en_el_tiempo(inputArg1,inputArg2)
+%PROCESAMIENTO_EN_EL_TIEMPO Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
