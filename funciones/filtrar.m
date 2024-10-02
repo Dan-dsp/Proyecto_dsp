46233be9e2c0757e547eb93b74d84198d17951a6
ADMIN <JuanDavidBeltran>
2024-10-01 23:06:16 -0500

    Filtrar supuestamente terminado

--- funciones/filtrar.m
+++ funciones/filtrar.m
@@ -1,4 +1,4 @@
-function [filtrada,t,datain,freqmuestreoin] = Filtrar(entrada,selectedItem,graficain,signalsData,windowsize)
+function [filtrada,datain,freqmuestreoin] = filtrar(entrada,selectedItem,graficain,signalsData,windowsize,filtroSeleccionado,app)
                      
             
 
@@ -12,118 +12,97 @@ function [filtrada,t,datain,freqmuestreoin] = Filtrar(entrada,selectedItem,grafi
             t= 0:1/signalData{2}:(length(signalData{1})-1)/signalData{2};%vector de tiempos para graficar
             plot(graficain, t, signalData{1});
             ventana = windowsize;
-            
-
-    selectedButton = app.FilterButtonGroup.SelectedObject;
-    
-    switch selectedButton.Text
+           
+     
+    switch filtroSeleccionado
         case 'FIR1'
-            % Código para aplicar FIR1
-        case 'FIR2'
-            % Código para aplicar FIR2
+            h= transpose (readmatrix('Filtros_Prueba_Proyecto1_Entregable.xlsx', 'Sheet','Filtro FIR 1'));
+            filtrada= convolucion(app, signalData{1}, h, ventana, signalData{2}); %convoluciona por bloques
+        case 'FIR2'            
+            h= transpose (readmatrix('Filtros_Prueba_Proyecto1_Entregable.xlsx', 'Sheet','Filtro FIR 2'));
+            filtrada= convolucion(app, signalData{1}, h, ventana, signalData{2}); %convoluciona por bloques
         case 'FIR3'
-            % Código para aplicar FIR3
+             h= transpose (readmatrix('Filtros_Prueba_Proyecto1_Entregable.xlsx', 'Sheet','Filtro FIR 3'));
+             filtrada=convolucion(app, signalData{1}, h, ventana, signalData{2}); %convoluciona por bloques
+        case 'FIR4'
+            h= transpose (readmatrix('Filtros_Prueba_Proyecto1_Entregable.xlsx', 'Sheet','Filtro FIR 4'));
+            filtrada=convolucion(app, signalData{1}, h, ventana, signalData{2}); %convoluciona por bloques
+        case 'FIR(n)'
+            h= app.valor_usuario;
+            filtrada=convolucion(app, signalData{1}, h, ventana, signalData{2}); %convoluciona por bloques
         case 'IIR1'
-            % Código para aplicar IIR1
-    end
+            T = readtable('Filtros_Prueba_Proyecto1_Entregable.xlsx','Sheet','Filtro IIR 1');
+                    valores= cellfun(@str2double,T.Valores);
+                    coeficiente= [T.Coeficiente];
+                    okb= 1;
+                    oka= 1;
+                    for i= 1:length(valores)
+                        if contains(coeficiente{i}, 'b')
+                           b(okb)=valores(i);
+                           okb= okb+1;
+                        elseif contains(coeficiente{i}, 'a')
+                             a(oka)=valores(i);
+                             oka= oka+1;
+                        end
+                    end
+             filtrada= ec_diference(app,signalData{1}, a, b, signalData{2});
 
+        case 'IIR2'            
+                   T = readtable('Filtros_Prueba_Proyecto1_Entregable.xlsx','Sheet','Filtro IIR 2');
+                    valores= cellfun(@str2double,T.Valores);
+                    coeficiente= [T.Coeficiente];
+                    okb= 1;
+                    oka= 1;
+                    for i= 1:length(valores)
+                        if contains(coeficiente{i}, 'b')
+                           b(okb)=valores(i);
+                           okb= okb+1;
+                        elseif contains(coeficiente{i}, 'a')
+                             a(oka)=valores(i);
+                             oka= oka+1;
+                        end
+                    end
+             filtrada= ec_diference(app,signalData{1}, a, b, signalData{2});
+             
+        case 'IIR3'
+            T = readtable('Filtros_Prueba_Proyecto1_Entregable.xlsx','Sheet','Filtro IIR 3');
+                    valores= cellfun(@str2double,T.Valores);
+                    coeficiente= [T.Coeficiente];
+                    okb= 1;
+                    oka= 1;
+                    for i= 1:length(valores)
+                        if contains(coeficiente{i}, 'b')
+                           b(okb)=valores(i);
+                           okb= okb+1;
+                        elseif contains(coeficiente{i}, 'a')
+                             a(oka)=valores(i);
+                             oka= oka+1;
+                        end
+                    end
+             filtrada= ec_diference(app,signalData{1}, a, b, signalData{2});
+             
+        case 'IIR4'
+             T = readtable('Filtros_Prueba_Proyecto1_Entregable.xlsx','Sheet','Filtro IIR 4');
+                    valores= cellfun(@str2double,T.Valores);
+                    coeficiente= [T.Coeficiente];
+                    okb= 1;
+                    oka= 1;
+                    for i= 1:length(valores)
+                        if contains(coeficiente{i}, 'b')
+                           b(okb)=valores(i);
+                           okb= okb+1;
+                        elseif contains(coeficiente{i}, 'a')
+                             a(oka)=valores(i);
+                             oka= oka+1;
+                        end
+                    end
+             filtrada= ec_diference(app,signalData{1}, a, b, signalData{2});
+             
+        case 'IIR(n)'
 
-            
-            if app.FIR1Button.Value
-                    h= transpose (readmatrix('Filtros_Prueba_Proyecto1_Entregable.xlsx', 'Sheet','Filtro FIR 1'));
-                    app.filtrada= convolucion(app, signalData{1}, h, ventana, signalData{2}); %convoluciona por bloques
-                    
-            elseif app.FIR2Button.Value
-                    h= transpose (readmatrix('Filtros_Prueba_Proyecto1_Entregable.xlsx', 'Sheet','Filtro FIR 2'));
-                    app.filtrada= convolucion(app, signalData{1}, h, ventana, signalData{2}); %convoluciona por bloques
-                    
-            elseif app.FIR3Button.Value
-                    h= transpose (readmatrix('Filtros_Prueba_Proyecto1_Entregable.xlsx', 'Sheet','Filtro FIR 3'));
-                    app.filtrada=convolucion(app, signalData{1}, h, ventana, signalData{2}); %convoluciona por bloques
-                    
-            elseif app.FIR4Button.Value
-                    h= transpose (readmatrix('Filtros_Prueba_Proyecto1_Entregable.xlsx', 'Sheet','Filtro FIR 4'));
-                    app.filtrada=convolucion(app, signalData{1}, h, ventana, signalData{2}); %convoluciona por bloques
-                    
-            elseif app.FIRnButton.Value
-                    h= app.valor_usuario;
-                    app.filtrada=convolucion(app, signalData{1}, h, ventana, signalData{2}); %convoluciona por bloques                   
-                  
-            elseif app.IIR1Button.Value 
-                    T = readtable('Filtros_Prueba_Proyecto1_Entregable.xlsx','Sheet','Filtro IIR 1');
-                    valores= cellfun(@str2double,T.Valores);
-                    coeficiente= [T.Coeficiente];
-                    okb= 1;
-                    oka= 1;
-                    for i= 1:length(valores)
-                        if contains(coeficiente{i}, 'b')
-                           b(okb)=valores(i);
-                           okb= okb+1;
-                        elseif contains(coeficiente{i}, 'a')
-                             a(oka)=valores(i);
-                             oka= oka+1;
-                        end
-                    end
-             app.filtrada= ec_diference(app,signalData{1}, a, b, signalData{2});
-             
-              elseif app.IIR2Button.Value 
-                    T = readtable('Filtros_Prueba_Proyecto1_Entregable.xlsx','Sheet','Filtro IIR 2');
-                    valores= cellfun(@str2double,T.Valores);
-                    coeficiente= [T.Coeficiente];
-                    okb= 1;
-                    oka= 1;
-                    for i= 1:length(valores)
-                        if contains(coeficiente{i}, 'b')
-                           b(okb)=valores(i);
-                           okb= okb+1;
-                        elseif contains(coeficiente{i}, 'a')
-                             a(oka)=valores(i);
-                             oka= oka+1;
-                        end
-                    end
-             app.filtrada= ec_diference(app,signalData{1}, a, b, signalData{2});
-             
-             elseif app.IIR3Button.Value
-                    T = readtable('Filtros_Prueba_Proyecto1_Entregable.xlsx','Sheet','Filtro IIR 3');
-                    valores= cellfun(@str2double,T.Valores);
-                    coeficiente= [T.Coeficiente];
-                    okb= 1;
-                    oka= 1;
-                    for i= 1:length(valores)
-                        if contains(coeficiente{i}, 'b')
-                           b(okb)=valores(i);
-                           okb= okb+1;
-                        elseif contains(coeficiente{i}, 'a')
-                             a(oka)=valores(i);
-                             oka= oka+1;
-                        end
-                    end
-             app.filtrada= ec_diference(app,signalData{1}, a, b, signalData{2});
-             
-             elseif app.IIR4Button.Value
-                    T = readtable('Filtros_Prueba_Proyecto1_Entregable.xlsx','Sheet','Filtro IIR 4');
-                    valores= cellfun(@str2double,T.Valores);
-                    coeficiente= [T.Coeficiente];
-                    okb= 1;
-                    oka= 1;
-                    for i= 1:length(valores)
-                        if contains(coeficiente{i}, 'b')
-                           b(okb)=valores(i);
-                           okb= okb+1;
-                        elseif contains(coeficiente{i}, 'a')
-                             a(oka)=valores(i);
-                             oka= oka+1;
-                        end
-                    end
-             app.filtrada= ec_diference(app,signalData{1}, a, b, signalData{2});
-             
-            elseif app.IIRkButton.Value
-                 
-                    b = app.valor_usuariobk;
+               b = app.valor_usuariobk;
                     a= app.valor_usuarioak;
                     
-              app.filtrada= ec_diference(app,signalData{1}, a, b, signalData{2});
+             filtrada= ec_diference(app,signalData{1}, a, b, signalData{2});
                    
-            end
-
-        end
+    end
