f6bcfadc7663b98276f4c25bdf298a6ddd476c53
Dan-dsp <daniel.pelaez@correounivalle.edu.co>
2024-10-05 15:37:58 -0500

    Las señales ya se pueden llevar de una tab a la otra

--- funciones/graficar.m
+++ funciones/graficar.m
@@ -1,4 +1,5 @@
-function graficar(axes, specific_title, signal, number_of_points)
+function graficar(axes, specific_title, signal)
+    number_of_points = (1:length(signal));
     stem(axes, number_of_points, signal);
     title(axes, specific_title);
     xlabel(axes, 'n');
