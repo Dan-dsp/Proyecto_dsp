0e6135254f2799a1dd3eb2a3cc655f03aa8613f9
Dan-dsp <daniel.pelaez@correounivalle.edu.co>
2024-10-05 16:08:53 -0500

    Cambios en la ubicación de dos archivos

--- drop_down_value_signal.m
+++
@@ -1,7 +0,0 @@
-function [app_signal, app_sample_rate] = drop_down_value_signal(app_SealdeentradaDropDown_Items, app_SealdeentradaDropDown_Value, app_signalsData)
-    selectedSignalIndex = strcmp(app_SealdeentradaDropDown_Items, app_SealdeentradaDropDown_Value);
-    signalData = app_signalsData{selectedSignalIndex};  % Retrieve signal and sample rate
-    app_signal = signalData{1};
-    app_sample_rate = signalData{2};
-end
-
