1f9e3952b2b2e59aaf8acd03606c7b1e681b3643
ADMIN <JuanDavidBeltran>
2024-10-05 17:46:51 -0500

    Interfaz acomodada, ajuste de volumen completado

--- funciones/Ajustevolumen.m
+++ funciones/Ajustevolumen.m
@@ -1,21 +1,33 @@
-function [prosig,prosigFs] = Ajustevolumen(datain,Fsin,potenciometro,modo,axes)
-volume = potenciometro/100;
-             
-             
-             if modo == 'Amplitud constante'
-                 prosig = datain*volume;
-             else
-                 if modo == 'Amplitud linealmente creciente'
-                    sizz = length(datain);
-                    volm = linspace(0,volume,sizz);
-                    prosig = datain .* volm;
-                 elseif modo == 'Amplitud linealmente decreciente'
-                     sizz = length(datain);
-                    volm = linspace(volume,0,sizz);
-                    prosig = datain .* volm;
-                 end
-             end
-             
-             prosigFs = Fsin;
-             plot(axes,prosig)
+function [prosig,prosigFs] = Ajustevolumen(datain, Fsin, potenciometro, modo)
+    % Normalización del valor del potenciometro
+    volume = potenciometro / 100;
+    
+    % Obtener el tamaño de la señal de entrada
+    [numSamples, numChannels] = size(datain);  % numChannels será 1 para mono y >1 para estéreo
+    
+    % Ajuste de volumen según el modo
+    if strcmp(modo, 'Amplitud constante')
+        prosig = datain * volume;
+        
+    elseif strcmp(modo, 'Amplitud linealmente creciente')
+        % Genera el vector de volumen que crece linealmente de 0 a volume
+        volm = linspace(0, volume, numSamples)';
+        
+        % Si la señal es estéreo (más de 1 canal), replicamos el vector de volumen
+        volm = repmat(volm, 1, numChannels);  % Ajusta el tamaño de volm para que coincida con datain
+        
+        % Aplicar ajuste de volumen a la señal
+        prosig = datain .* volm;
+        
+    elseif strcmp(modo, 'Amplitud linealmente decreciente')
+        % Genera el vector de volumen que decrece linealmente de volume a 0
+        volm = linspace(volume, 0, numSamples)';
+        
+        % Si la señal es estéreo (más de 1 canal), replicamos el vector de volumen
+        volm = repmat(volm, 1, numChannels);  % Ajusta el tamaño de volm para que coincida con datain
+        
+        % Aplicar ajuste de volumen a la señal
+        prosig = datain .* volm;
+        prosigFs = Fsin;
+    end
 end
